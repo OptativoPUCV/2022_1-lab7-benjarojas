1debab02f32042be00ccd7fa9a2a4eb4831961d7
Your Name <you@example.com>
2022-05-11 01:06:01 -0400

    new try

--- heap.c
+++ heap.c
@@ -18,7 +18,8 @@ typedef struct Heap{
 
 
 void* heap_top(Heap* pq){
-    return pq->heapArray[0].data;
+   if(pq->size == 0) return NULL;
+   return pq->heapArray[0].data;
 }
 
 void heap_push(Heap* pq, void* data, int priority){
