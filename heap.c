5428c7b73aef554e0000657d5f7b48407a82590e
benjarojas <benjathaboi@gmail.com>
2022-05-11 17:15:30 -0400

    new try

--- heap.c
+++ heap.c
@@ -64,6 +64,8 @@ void heap_push(Heap* pq, void* data, int priority){
 
 void heap_pop(Heap* pq){
    _swap(&(pq->heapArray[0]), &(pq->heapArray[pq->size-1]));
+   pq->heapArray[pq->size-1].priority = 0;
+   pq->heapArray[pq->size-1].data = NULL;
 }
 
 Heap* createHeap(){
